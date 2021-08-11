cf78f662b3056ca7ef2876696c21e33be2a38afd
Suryo Prasetyo W <the.oyrus@gmail.com>
2021-08-11 14:41:42 +0700

    init: openmusic app consumer done

---
+++ src/PlaylistsService.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylists(playlistId) {
+    const query = {
+      text: `SELECT songs.* FROM songs
+      JOIN playlistsongs ON songs.id = playlistsongs.song_id 
+      WHERE playlistsongs.playlist_id = $1`,
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+}
+
+module.exports = PlaylistsService;
